6c1f84ac96377fd0e321efa71bb397fb57dfeebb
Guillermo Cique <guillermo@holvi.com>
2020-11-16 14:17:06 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MessagingSDK",
+    name: "ZendeskMessagingSDK",
     products: [
         .library(name: "MessagingSDK", targets: ["MessagingSDK"])
     ],
