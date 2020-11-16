bce1c83f2faa66a549747921f0c0e51c518ca78a
Guillermo Cique <guillermo@holvi.com>
2020-11-16 16:24:23 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,10 +8,6 @@ let package = Package(
         .library(name: "MessagingSDK", targets: ["MessagingSDK"])
     ],
     targets: [
-        .binaryTarget(
-            name: "MessagingSDK",
-            url: "https://zendesk.jfrog.io/zendesk/libs-releases-local/ios/zendesk/5.2.4/MessagingSDK/3.8.0/MessagingSDK.zip",
-            checksum: "f05458e5795c380d60feb3474cc9944a310f5d6398a909333c919c5c71361974"
-        )
+        .binaryTarget(name: "MessagingSDK", path: "MessagingSDK.xcframework")
     ]
 )
